6e441985bd0eff8d7ce9bfe3f132ef938703a728
Matt <matthew.wong@outlook.com>
2018-10-11 19:12:29 -0400

    Corrected syntax error and now shows a basic page. Added missing .babelrc file

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -22,24 +22,25 @@ module.exports = {
         test: /\.js|.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          
         }
       },
       {
-          test: /\.scss$/,
-            use: [
-              'style-loader',
-              {
-                loader: 'css-loader',
-                query: {
-                  modules: true,
-                  sourceMap: true,
-                  importLoaders: 2,
-                  localIdentName: '[name]__[local]___[hash:base64:5]'
-                }
-              },
-              'sass-loader'
-            ]
+        test: /\.scss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 2,
+                localIdentName: '[name]__[local]___[hash:base64:5]'
+              }
+            },
+            'sass-loader'
+          ]
         /*
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class App extends React.Component {
+	
 	render(){
-		return();
+		const element = <h1>Biscuit</h1>;
+		return(element)
 	}
 }
 ReactDOM.render(
 	(
 		<App />
+		// React.createElement('div',null,'HEllo world')
+		
 	
-), document.getElementById('app'));
+), document.getElementById('root'));
 
 module.hot.accept();
