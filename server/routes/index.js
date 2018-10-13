de4d5d0b76e6671572f2a8b43aa4b19631f0f816
Matt <matthew.wong@outlook.com>
2018-10-12 20:54:35 -0400

    Made it able to run Webpack and Express at the same time

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,12 @@ const path = require('path');
 const router = require('express').Router();
 
 router
-  .get('/*', (req, res, next) => {
+  .get('/', (req, res, next) => {
     const routePath = path.join(__dirname + '..', '..', '..', 'dist/' + 'index.html');
     res.sendFile(routePath);
+  })
+  .get('/test', (req, res, next) => {
+    res.send("dfsfsd");
   })
 
 module.exports = router;
