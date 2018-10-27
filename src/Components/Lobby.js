ff81de9c03beb6a168e05502c297deec3e885309
Matt <matthew.wong@outlook.com>
2018-10-27 19:13:00 -0400

    Added base lobby route

--- src/Components/Lobby.js
+++ src/Components/Lobby.js
@@ -6,23 +6,15 @@ let socket = io(`http://localhost:3000`)
 class Lobby extends React.Component{
     constructor(props){
         super(props)
-        //this.room = React.createRef()
-        //this.redirect = React.createRef()
-        //this.handleLobby = this.handleLobby.bind(this)
     }
 
-    handleLobby(event){
-        /*
-        let room = this.room.current.value
-        let obj = {"room":room}
-        console.log("this is room", obj)
-        */
+    LobbyRoom(event){
     }
 
     render(){
         return(
           <div>
-            <div id='code'>code: d</div>
+            <div id='code'>code: </div>
             <div id='players'>players: </div>
           </div>
         )
