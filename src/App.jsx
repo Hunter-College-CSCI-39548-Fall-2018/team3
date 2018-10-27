eedffa8ff4b2a309f288aae7fc6fb4db6be3ad98
Shelly Huang <shellyhuang81@gmail.com>
2018-10-27 12:41:01 -0400

    added paths in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './Components/Home'
 class App extends React.Component {
-
+// express has precedence over react- 
+// if 
 	render(){
 		return(
 			<BrowserRouter>
-				<Route path='/hi' component={Home} ></Route>
+				<Switch>
+					<Route path='/hi' component={Home}></Route>
+					<Route path='/p' component={Home}></Route>
+					
+					<Route path= '/test' component={Home}></Route>
+				</Switch>
+				
 			</BrowserRouter>
 		)
 	}
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            hi hello hoe
         </div>
     )
 }
