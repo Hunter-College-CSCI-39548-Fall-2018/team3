904b5b11117091849999daf9f25a39fed59c453a
Shelly Huang <shellyhuang81@gmail.com>
2018-11-04 19:20:15 -0500

    Reformated lobby component so it's neater

--- src/Components/Lobby.js
+++ src/Components/Lobby.js
@@ -1,45 +1,55 @@
 import React from 'react'
 import io from 'socket.io-client';
+
 class Lobby extends React.Component{
     constructor(props){
-      super(props)
+    super(props)
 
-      this.state = {players: ""}
+      this.state = {socket: false, players: ""}
+    }
+    componentDidMount(){
+        fetch('http://localhost:3000/lobby', {
+            method: 'GET',
+            credentials: 'include'
+        })
+        //fetch is asynchronous, so need to set socket state and then do all stuff after
+        .then((res) => {
+            console.log("res status is", res.status)
+            const socket = io.connect('http://localhost:3000/', {
+                transports: ['websocket'],
+                upgrade: false
+            })
+
+            this.setState({socket:socket})
+            this.handleEvents()
+         })
+        .catch((err) => {
+            console.log(err)
+        })
+        console.log("on first render?")
     }
 
-    componentDidMount(){
-      fetch('http://localhost:3000/lobby', {
-        method: 'GET',
-        credentials: 'include'
-      }).catch((err) => {
-        console.log(err)
-      })
+    handleEvents = () => {
+        let socket = this.state.socket
 
-      const socket = io.connect('http://localhost:3000/', {
-        transports: ['websocket'],
-        upgrade: false
-      })
-      console.log("on first render?")
+        socket.on('get-curr-users', (curr_users) => {
+            let players = ""
+            console.log('attempting to add current users')
 
-      socket.on('get-curr-users', (curr_users) => {
-        let players = ""
-        console.log('attempting to add current users')
+            for(let key of curr_users){
+                players += (" " + key)
+            }
 
-        for(let key of curr_users){
-          players += (" " + key)
-        }
+            this.setState({players: players})
+        })
 
-        this.setState({players: players})
-      })
+        socket.on('new-player', (name) => {
+          let player = ""
+          console.log('received new player')
+          player += (" " + name)
 
-      socket.on('new-player', (name) => {
-        let player = ""
-        console.log('received new player')
-        player += (" " + name)
-
-        this.setState({players: this.state.players + player})
-      })
-
+          this.setState({players: this.state.players + player})
+        })
     }
 
     render(){
@@ -50,6 +60,7 @@ class Lobby extends React.Component{
         </div>
       )
     }
+
 }
 
 export default Lobby
