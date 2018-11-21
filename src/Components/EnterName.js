acbacf794e015a17d1d62ee5652aeb7d62f2f96a
Matt <matthew.wong@outlook.com>
2018-11-20 22:31:43 -0500

    Finished check for name length and existence

--- src/Components/EnterName.js
+++ src/Components/EnterName.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
 
-class EnterName extends React.Component{
+class EnterName extends React.Component{ 
     constructor(props){
         super(props)
         this.nickname = React.createRef()
@@ -15,6 +15,11 @@ class EnterName extends React.Component{
         let name = this.nickname.current.value
         let obj = {"nickname":name}
 
+        if(name.length > 12 || name.length < 3){
+            document.getElementById("name-length").style.display="inline";
+            return;
+        }
+        
         console.log("this is name", obj)
         let host = 'http://' + location.hostname
         fetch(host+':3000/enter-name', {
@@ -24,7 +29,7 @@ class EnterName extends React.Component{
             credentials: 'include'
         }).then(res => {
             if(res.ok){
-                //parse through response's json object first to get value
+                //parse through response's json object first to get value 
                 res.json()
                 .then((body) =>{
                     console.log('this is the response', body)
@@ -32,6 +37,11 @@ class EnterName extends React.Component{
                     if(body){
                         this.setState({redirect: true})
                         console.log("state of redirect:",this.state.redirect)
+                        
+                    }
+                    else{
+                        document.getElementById("name-taken").style.display="inline";
+                        return;
                     }
                 })
             }
@@ -52,6 +62,10 @@ class EnterName extends React.Component{
 
                     <input ref={this.nickname} type='text' name='nickname' autoFocus/>
                     <button id='enter-name' type='button' onClick={this.handleEnterName}>Next</button>
+                    <ul>
+                        <li id="name-length" style={{display:"none"}}>Nickname must be 3-12 characters</li>
+                        <li id="name-taken" style={{display:"none"}}>Name has already been taken</li>
+                    </ul>
                 </div>
 
             )
