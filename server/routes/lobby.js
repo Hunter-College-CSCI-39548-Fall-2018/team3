a8f13a5165fde256dd89ac7abb3e0c2ee5841c99
Matthew Wong <matthew.wong@outlook.com>
2018-11-23 00:02:03 -0500

    Attempt again with updated code

--- server/routes/lobby.js
+++ server/routes/lobby.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-module.exports = (app, io, rooms) => {
-    var curr_users = []
-    app.get('/lobby', (req, res) => {
-        console.log("lobby post was called")
-
-        io.sockets.on('connection', (socket) => {
-            console.log("is room in cookie", req.cookies)
-            if(req.cookies.game_owner == '0'){
-                var player = rooms[req.cookies.room].players[req.cookies.player]
-
-                var name = req.cookies.player
-                var room = req.cookies.room
-
-                console.log("if player connected", player.connected)
-                //make sure to emit user has joined only once
-                if(!player.connected){
-                    socket.emit('get-curr-users', curr_users)
-                    curr_users.push(name)
-
-                    //save state of users in lobby
-                    player.connected = true
-
-                    //join when get to lobby
-                    //rooms are forgotten after reroute
-                    socket.join(room)
-                    console.log('joined the room:', room)
-                    socket.to(room).emit('new-player', name)
-                }
-            }
-            else if(req.cookies.game_owner == '1'){
-                //just so game owner is in the room and can see what's going on
-                socket.join(req.cookies.room)
-            }
-
-            
-
-        })
-        
-        res.sendStatus(200)
-    })
-}
-=======
 module.exports = (app, io, rooms) => {
 
     function startTimer(){
@@ -71,7 +28,7 @@ module.exports = (app, io, rooms) => {
 
             // tell everyone that player has joined, get current users in lobby
             socket.emit('get-curr-users', rooms[room].players)
-
+            rooms[room].addPlayer(name, name)
             player.socketid = socket.id
             socket.join(room)
 
@@ -149,4 +106,3 @@ module.exports = (app, io, rooms) => {
         res.sendStatus(200)
     })
 }
->>>>>>> 7ec00cf093d05c662c885269f17314a321970d7d
