cede057e47fa16180b38ce04c59165adcc768418
michelleli723 <michelleli94237@gmail.com>
2018-12-02 21:40:04 -0500

    export functions for icon generator

--- server/routes/utils/teams.js
+++ server/routes/utils/teams.js
@@ -1,4 +1,4 @@
-class Team{
+class Team {
     constructor(){
         this.players = []
         this.curr_icon = 1
@@ -7,3 +7,8 @@ class Team{
 }
 
 module.exports = Team
+
+//room.teams[0].players.length
+//for num in players //objects //gives keys in object
+//for num of players //iterate arrays //gives what's inside the arrays as in "a", "b", "c"
+//for num in players //iterate arrays //gives you position as in 0,1,2
